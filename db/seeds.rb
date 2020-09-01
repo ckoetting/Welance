310650acf2c980e2f8441a9fdcc58ce5a78e91c9
Mohamed Soliman <mosoliman4@gmail.com>
2020-09-01 14:58:46 +0200

    footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting seeds.."
+User.destroy_all
+
+#User.create(email: "filo.filo@filo.filo", full_name: "Filo Lanza",  ,  dob: 11-01-1993,  , )
+User.create(email: "filo.filo@filo.filo", password:"filofilo", full_name: "Filo Donna", user_type: "Developer", bio: "I love loving love", location: "Naboo", phone_number: "3343421236", hourly_rate: "50 €", headline: "headline", business_address: "Business Adress 1234", business_name: "Business Name", business_vat_id: 5, business_size: "50", website: "filo.filo.com" )
+puts "#{User.count} users created"
