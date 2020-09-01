2257374fe02f3535c8640fcff13e306997d7b430
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-09-01 14:21:14 +0200

    basic landing page design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -31,4 +32,10 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+});
+
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
 });
