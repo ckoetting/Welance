de7fa9535126239744fb7a68f55044193462ee01
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-09-03 12:47:28 +0200

    fixed a syntax error in routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-
   def developers
     @developers = User.where(user_type: "developer")
   end
 
   def businesses
     @businesses = User.where(user_type: "business")
+  end
+
+  def job_inquiries
+    @job_inquiries = Inquiry.where()
   end
 end
