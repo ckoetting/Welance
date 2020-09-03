8d5e2b9d2c9cac1153b699a08910586b7e159c82
Filippo Lanza <fpg.lanza@gmail.com>
2020-09-03 16:49:23 +0200

    added save job offer functionality, needs lil fixin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  post '/bookmark/:id' => 'offers#bookmark', as: :bookmark
   resources :offers do
     resources :inquiries, shallow: true
     resources :reviews
     end
   get '/users/my_inquiries', to: 'inquiries#index', as: :my_inquiries
   get 'users/applications', to: 'users#job_applications', as: :business_applications
-  resources :users, only: [ :show ] 
+  resources :users, only: [ :show ]
   resources :contact, only: [:new, :create]
   get 'contact_us' => 'pages#contact_us'
 end
