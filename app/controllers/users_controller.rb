49595d6dcfc3307050804a754ac4c1ec37baa608
Cornelius Koetting <cornelius@ckdevelopment.de>
2020-09-03 10:30:00 +0200

    Users show updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def developers
     @developers = User.where(user_type: "developer")
   end
