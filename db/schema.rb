ee313b0cd1fe3e22b1ca95800ef7338baaefe629
Felix Hermann <felixhermann@MacBook-Pro-2.localdomain>
2020-09-04 11:51:20 +0200

    search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_152827) do
+ActiveRecord::Schema.define(version: 2020_09_04_094414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,8 +66,6 @@ ActiveRecord::Schema.define(version: 2020_09_03_152827) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "location_list"
-    t.string "skill_list"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
