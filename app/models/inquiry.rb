68fcbb5dad7617e9ceaf85f015898f46719cfa9c
Mohamed Soliman <mosoliman4@gmail.com>
2020-09-07 17:15:39 +0200

    seeds compeleted

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,7 +1,7 @@
 class Inquiry < ApplicationRecord
   belongs_to :user
   belongs_to :offer
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :status, presence: true
   validates :message, presence: true
 end
