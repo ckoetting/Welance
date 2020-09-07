5a385c607278851e43c7a7079a9734eac0efff33
HC Tung <hc.tung@gmail.com>
2020-09-07 17:28:25 +0200

    changed a lot of styling issues, also added gradient corners to cards, avatar in navbar, realigned and fixed landing page width, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'users/profile_sign_up', to: 'users/registrations#profile_sign_up', :via => [:get], :as => 'profile_sign_up'
+  end
   root to: 'pages#home'
   post '/bookmark/:id' => 'offers#bookmark', as: :bookmark
   resources :offers do
     resources :inquiries, shallow: true
     resources :reviews
-    end
+  end
   get '/users/my_inquiries', to: 'inquiries#index', as: :my_inquiries
   get 'users/applications', to: 'users#job_offers', as: :business_applications
   get 'users/success', to: 'users#success', as: :inquiry_sent
@@ -14,10 +17,11 @@ devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'
   resources :contact, only: [:new, :create]
   resources :about, only: [:show]
   get 'contact_us' => 'pages#contact_us'
-  get 'about' => 'pages#about'
   resources :services, only: [:show]
   get 'services' => 'pages#services'
-  get 'services' => 'pages#services'
+  get 'about' => 'pages#about'
+  resources :customers, only: [:show]
+  get 'customers' => 'pages#customers'
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
